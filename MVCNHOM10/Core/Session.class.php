d55e82733685ae203a0f74d57324158ae19ee22e
Anh123 <91522785+Anhh123@users.noreply.github.com>
2021-11-23 14:06:23 +0700

    ducanh

---
+++ MVCNHOM10/Core/Session.class.php
@@ -0,0 +1,46 @@
+<?php
+
+    session_start();
+
+
+    class Session {
+
+        public static function IsAdminLogged(){
+            return Session::Get("admin_id") != null;
+        }
+
+        public static function IsLogged(){
+            return Session::Get("user_id") !== null;
+        }
+        
+        public static function SetLoggedID($id){
+            Session::Set("user_id", $id);
+        }
+
+        public static function SetIDAdminLogged($id){
+            Session::Set("admin_id", $id);
+        }
+
+        public static function GetIDLogged(){
+            return Session::Get("user_id");
+        }
+
+        public static function GetIDAdminLogged(){
+            return Session::Get("admin_id");
+        }
+
+        public static function Get($name){
+            return isset($_SESSION[$name]) ? $_SESSION[$name] : null;
+        }
+
+        public static function Set($name, $value){
+            $_SESSION[$name] = $value;
+        }
+
+        public static function DestroyAll(){
+            session_destroy();
+        }
+
+    }
+
+?>
